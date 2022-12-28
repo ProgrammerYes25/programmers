762c82c4200f47672bdcc7bf811faf1859c0a1b7
yscho010612@gmail.com <yscho010612@gmail.com>
2022-12-28 22:54:04 +0900

    2022.12.28 - 두 수의 합

--- src/Lv0_2022_12_28_No_1.java
+++ src/Lv0_2022_12_28_No_1.java
@@ -1,4 +1,4 @@
-public class Lv0_2022_12_28 {
+public class Lv0_2022_12_28_No_1 {
 //    문제 설명
 //    정수 num1과 num2가 주어질 때, num1과 num2의
 //    합을 return하도록 soltuion 함수를 완성해주세요.
@@ -17,11 +17,16 @@ public class Lv0_2022_12_28 {
 //      num1이 2이고 num2가 3이므로 2 + 3 = 5를 return합니다.
 //    입출력 예 #2
 //      num1이 100이고 num2가 2이므로 100 + 2 = 102를 return합니다.
-    public int solution(int num1, int num2){
-        int answer = 0;
+
+    public static int solution(int num1, int num2){
+        int answer = num1+num2; // 합계 구하는 코드
         return answer;
     }
-    public static void main(String [] args){
-
+    public static void main(String[] args){
+        int result;
+        result = solution(2, 3);
+        System.out.println(result);
+        result = solution(100, 2);
+        System.out.println(result);
     }
 }
