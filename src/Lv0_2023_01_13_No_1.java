f1ecd96a1b8bc820f296a4c534155294842f5af9
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-01-13 23:02:02 +0900

    2023.01.13 - 배열 뒤집기

---
+++ src/Lv0_2023_01_13_No_1.java
@@ -0,0 +1,54 @@
+public class Lv0_2023_01_13_No_1 {
+//  문제 설명
+//  정수가 들어 있는 배열 num_list가 매개변수로 주어집니다.
+//  num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+//
+//  제한사항
+//  1 ≤ num_list의 길이 ≤ 1,000
+//  0 ≤ num_list의 원소 ≤ 1,000
+//
+//  입출력 예
+//  |       num_list	    |       result          |
+//  |   [1, 2, 3, 4, 5]     |	 [5, 4, 3, 2, 1]    |
+//  |  [1, 1, 1, 1, 1, 2]	|  [2, 1, 1, 1, 1, 1]   |
+//  | [1, 0, 1, 1, 1, 3, 5]	| [5, 3, 1, 1, 1, 0, 1] |
+//
+//  입출력 예 설명
+//  입출력 예 #1
+//  num_list가 [1, 2, 3, 4, 5]이므로 순서를 거꾸로 뒤집은 배열 [5, 4, 3, 2, 1]을 return합니다.
+//  입출력 예 #2
+//  num_list가 [1, 1, 1, 1, 1, 2]이므로 순서를 거꾸로 뒤집은 배열 [2, 1, 1, 1, 1, 1]을 return합니다.
+//  입출력 예 #3
+//  num_list가 [1, 0, 1, 1, 1, 3, 5]이므로 순서를 거꾸로 뒤집은 배열 [5, 3, 1, 1, 1, 0, 1]을 return합니다.
+
+    public static int[] solution(int[] n){
+        int answer[] = new int[n.length];
+        int left =0, right = n.length-1;
+        while (left<=right){
+            answer[right] = n[left];
+            answer[left++] = n[right--];
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        int[] result;
+        int[] num1 = {1, 2, 3, 4, 5};
+        result = solution(num1);
+        for(int i : result){
+            System.out.print(i+", ");
+        }
+        System.out.println();
+        int[] num2 = {1, 1, 1, 1, 1, 2};
+        result = solution(num2);
+        for(int i : result){
+            System.out.print(i+", ");
+        }
+        System.out.println();
+        int[] num3 = {1, 0, 1, 1, 1, 3, 5};
+        result = solution(num3);
+        for(int i : result){
+            System.out.print(i+", ");
+        }
+        System.out.println();
+    }
+}
